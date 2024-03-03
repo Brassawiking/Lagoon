165faf0dcfc4049133b2753257b84496430086b8
Brassawiking <brassawiking@gmail.com>
2024-03-03 13:16:45 +0100

    Mapping out Atland

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import { ref, debugLiveWatchers, debugRefCounter } from '../framework/framework.js'
+import { ref, debugLiveWatchers, debugRefCounter, compareArrays } from '../framework/framework.js'
 import { clamp, mouseDrag } from './utils.js'
 import rawData from '../data/data.json' assert { type: 'json' }
 
@@ -8,11 +8,16 @@ const TILE_SIZE = 16
 
 const MOUSE_LEFT_BUTTON_BIT = 1
 
+const TOOL_MOVE = 'Move'
+const TOOL_PAINT = 'Paint'
+
 const data = structuredClone(rawData)
 let viewOriginX = -50
 let viewOriginY = -50
 let viewZoom = 100
-let currentPaintingTile = 1
+let currentMap
+let currentTool = TOOL_PAINT
+let currentPaintingTile = 0
 window.x = data
 
 const handleWheelZoom = (event) => {
@@ -36,9 +41,14 @@ const handleMousedownPan = (event) => {
 }
 
 const handleMousedownMap = (event, map, $element) => {
+  if (currentMap !== map || currentTool !== TOOL_MOVE) {
+    return
+  }
+  
   mouseDrag(event, MOUSE_LEFT_BUTTON, () => {
     const x = map.x
     const y = map.y
+    // TODO: Style or remove
     $element.classList.add('dragged')
 
     return {
@@ -54,26 +64,43 @@ const handleMousedownMap = (event, map, $element) => {
 }
 
 const addMap = () => {
+  const width = 8
+  const height = 8
   data.maps = [
     ...data.maps,
-    {
-      name: 123,
-      width: 8,
-      height: 8,
-      x: 5,
-      y: 34,
-      tilemap: []
+    currentMap = {
+      name: `New map (${data.maps.length + 1})`,
+      width,
+      height,
+      x: 0,
+      y: 0,
+      tilemap: Array(width * height).fill(currentPaintingTile)
     }
   ]
 }
 
 const removeMap = () => {
-  data.maps = data.maps.slice(0, -1)
+  if (confirm('Are you sure you want to remove map?')) {
+    data.maps = data.maps.filter((map) => map !== currentMap)
+    currentMap = null
+  }
+}
+
+const updateMapSize = (width, height) => {
+  if (!currentMap) {
+    return
+  }
+
+  const newTilemap = Array(width * height).fill(currentPaintingTile)
+  currentMap.width = width
+  currentMap.height = height
+  currentMap.tilemap = newTilemap
 }
 
 document.querySelector('#app').innerHTML = `
   <div class="world-editor">
     <div class="world-editor--view" ${ref()
+      .on('click', () => currentMap = null)
       .on('mousedown', handleMousedownPan)
       .on('wheel', handleWheelZoom)
     }>
@@ -82,11 +109,16 @@ document.querySelector('#app').innerHTML = `
         .style('translate', () => `${-viewOriginX}px ${-viewOriginY}px`)
         .repeat(() => data.maps, (map) => `
           <div class="world-editor--map" ${ref()
-            .on('mousedown', (event, el) => handleMousedownMap(event, map, el))
+            .class('current', () => map === currentMap)
             .style('width', () => `${map.width * TILE_SIZE}px`)
             .style('height', () => `${map.height * TILE_SIZE}px`)
             .style('left', () => `${map.x * TILE_SIZE}px`)
             .style('top', () => `${map.y * TILE_SIZE}px`)
+            .on('click', (event) => {
+              event.stopPropagation()
+              currentMap = map
+            })
+            .on('mousedown', (event, el) => handleMousedownMap(event, map, el))
           }>
             <div class="world-editor--map-name" ${ref()
               .property('innerText', () => map.name)
@@ -98,12 +130,18 @@ document.querySelector('#app').innerHTML = `
                   .attribute('x', () => (index % map.width) * TILE_SIZE)
                   .attribute('y', () => (index / map.width | 0) * TILE_SIZE)
                   .on('mousedown', (event) => {
+                    if (currentMap !== map || currentTool !== TOOL_PAINT) {
+                      return
+                    }
                     if (event.button === MOUSE_LEFT_BUTTON) {
                       event.stopPropagation()
                       map.tilemap[index] = currentPaintingTile
                     }
                   })
                   .on('mouseover', (event) => {
+                    if (currentMap !== map || currentTool !== TOOL_PAINT) {
+                      return
+                    }
                     if (event.buttons & MOUSE_LEFT_BUTTON_BIT) {
                       map.tilemap[index] = currentPaintingTile
                     }
@@ -117,33 +155,109 @@ document.querySelector('#app').innerHTML = `
     </div>
 
     <div class="world-editor--controls">
-      <div class="canvas-stats">
-        <div ${ref().property('innerText', () => `Number of refs: ${debugRefCounter()}`)}></div>
-        <div ${ref().property('innerText', () => `Live watchers: ${debugLiveWatchers()}`)}></div>
-        <div ${ref().property('innerText', () => `View origin: ${viewOriginX}, ${viewOriginY}`)}></div>
-        <div ${ref().property('innerText', () => `Zoom: ${viewZoom}%`)}></div>
+      <fieldset>
+        <legend>Toolbar</legend>
+
+        <div ${ref()
+          .property('innerText', () => `Current tool: ${currentTool}`)
+        }></div>
+
+        <button ${ref()
+          .on('click', () => currentTool = TOOL_MOVE)
+          .property('innerText', () => TOOL_MOVE)
+        }></button>
+
+        <button ${ref()
+          .on('click', () => currentTool = TOOL_PAINT)
+          .property('innerText', () => TOOL_PAINT)
+        }></button>
+        
+        <button ${ref()
+          .on('click', addMap)
+        }>
+          Add map
+        </button>
+
+        <button ${ref()
+          .on('click', () => {
+            console.log(JSON.stringify(data, null, 2))
+          })
+        }>
+          Copy JSON data
+        </button>
+      </fieldset>
+
+      <fieldset>
+        <legend>Info</legend>
+
+        <div>
+          <div ${ref().property('innerText', () => `View origin: ${viewOriginX}, ${viewOriginY}`)}></div>
+          <div ${ref().property('innerText', () => `Zoom: ${viewZoom}%`)}></div>
+          <div ${ref().property('innerText', () => `[Debug] Number of refs: ${debugRefCounter()}`)}></div>
+          <div ${ref().property('innerText', () => `[Debug] Live watchers: ${debugLiveWatchers()}`)}></div>
+        </div>
+      </fieldset>
+
+      <div ${ref()
+        .style('display', () => currentMap ? '' : 'none')
+        .repeat(() => currentMap ?  [currentMap] : [], () => `
+          <fieldset>
+            <legend>Current map selected</legend>
+
+            <div style="display: flex; flex-direction: column;">
+              <label>
+                Name:
+                <input ${ref()
+                  .property('value', () => currentMap.name)
+                  .on('input', (event) => currentMap.name = event.target.value)
+                }/>
+              </label>
+
+              <label>
+                Width:
+                <input type="number" step="0" min="1" ${ref()
+                  .property('value', () => currentMap.width)
+                  .on('change', (event) => updateMapSize(parseInt(event.target.value) || 1, currentMap.height))
+                }/>
+              </label>
+
+              <label>
+                Height:
+                <input type="number" step="0" min="1" ${ref()
+                  .property('value', () => currentMap.height)
+                  .on('change', (event) => updateMapSize(currentMap.width, parseInt(event.target.value) || 1))
+                }/>
+              </label>
+
+              <div ${ref().property('innerText', () => `X: ${currentMap.x}`)}></div>
+              <div ${ref().property('innerText', () => `Y: ${currentMap.y}`)}></div>
+            </div>
+            
+            <button ${ref().on('click', removeMap)}>
+              Remove map
+            </button>
+          </fieldset>
+        `, (x) => x, compareArrays)}>
       </div>
 
-      <button ${ref().on('click', addMap)}>
-        Add map
-      </button>
+      <fieldset>
+        <legend>Tiles</legend>
 
-      <button ${ref().on('click', removeMap)}>
-        Remove map
-      </button>
-
-      <fieldset style="display: flex; flex-direction: column; gap: 10px;">
-        <legend>Painting</legend>
-        <input type="number" step="0" ${ref()
-          .property('value', () => currentPaintingTile)
-          .on('input', (event) => {
-            currentPaintingTile = Number(event.target.value) || 0
-          })
-        }/>
-        <div ${ref().property('innerText', () => (data.tiles[currentPaintingTile] || data.tiles[0]).name)}></div>
-        <img style="image-rendering: pixelated;" ${ref()
-          .property('src', () => (data.tiles[currentPaintingTile] || data.tiles[0]).image)
-        }/>
+        <div class="tile-palette" ${ref()
+          .repeat(() => data.tiles, (tile, index) => `
+            <div class="tile-preview" ${ref()
+              .class('selected', () => index === currentPaintingTile)
+              .on('click', () => currentPaintingTile = index)
+            }>
+              <div ${ref()
+                .property('innerText', () => tile.image.split('.').at(0).split('/').at(-1))
+              }></div>
+              <img style="image-rendering: pixelated;" ${ref()
+                .property('src', () => tile.image)
+              }/>
+            </div>
+          `)}
+        ></div>
       </fieldset>
     </div>
   </div>
